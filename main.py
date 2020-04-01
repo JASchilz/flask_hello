0cb629a5ffcf12da28da0f2a83cbcde92a86ea6f
Joseph Schilz <joseph@schilz.org>
2020-03-31 20:08:47 -0700

    Add home page.

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def hello_world():
 def hello_name(name):
     return 'Hello {}!'.format(name)
 
+@app.route('/')
+def home():
+    return 'Welcome home!'
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
